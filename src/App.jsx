1a646f1a9963153f69d5608eb3e99184373f82f2
Ata Mohammadi <atasmohammadi@gmail.com>
2024-02-10 00:00:05 +0100

    update author

--- src/App.jsx
+++ src/App.jsx
@@ -10,86 +10,79 @@ import Home from "./pages/Home";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useDispatch, useSelector } from "react-redux";
 import {
-	registerUser,
-	loginUser,
-	logoutUser,
+  registerUser,
+  loginUser,
+  logoutUser,
 } from "./redux/reducers/usersSlice";
 import { createPost, deletePost, likePost } from "./redux/reducers/postsSlice";
 
 const App = () => {
-	const users = useSelector((state) => state.users.users);
-	const loggedInUser = useSelector((state) => state.users.loggedInUser);
-	console.log("loggedUser", loggedInUser);
-	const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+  const loggedInUser = useSelector((state) => state.users.loggedInUser);
+  console.log("loggedUser", loggedInUser);
+  const dispatch = useDispatch();
 
-	const handleRegisterUser = (username, email, password) => {
-		dispatch(registerUser({ username, email, password }));
-	};
+  const handleRegisterUser = (username, email, password) => {
+    dispatch(registerUser({ username, email, password }));
+  };
 
-	const handleLoginUser = (email, password) => {
-		dispatch(loginUser({ email, password }));
-	};
+  const handleLoginUser = (email, password) => {
+    dispatch(loginUser({ email, password }));
+  };
 
-	const handleLogoutUser = () => {
-		dispatch(logoutUser());
-	};
+  const handleLogoutUser = () => {
+    dispatch(logoutUser());
+  };
 
-	const handleCreatePost = (title, text) => {
-		dispatch(createPost({ title, text, author: loggedInUser }));
-	};
+  const handleCreatePost = (title, text) => {
+    dispatch(createPost({ title, text, author: loggedInUser.username }));
+  };
 
-	const handleDeletePost = (id) => {
-		dispatch(deletePost(id));
-	};
+  const handleDeletePost = (id) => {
+    dispatch(deletePost(id));
+  };
 
-	const handleLikePost = (id, type) => {
-		dispatch(likePost({ id, type }));
-	};
+  const handleLikePost = (id, type) => {
+    dispatch(likePost({ id, type }));
+  };
 
-	useEffect(() => {
-		console.log("users", users);
-	}, [users]);
+  useEffect(() => {
+    console.log("users", users);
+  }, [users]);
 
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Router>
-				<Routes>
-					<Route
-						path="/"
-						element={<Navbar logoutUser={handleLogoutUser} />}
-					>
-						<Route
-							path="/"
-							element={
-								<Home
-									users={users}
-									deletePost={handleDeletePost}
-									likePost={handleLikePost}
-								/>
-							}
-						/>
-						<Route
-							path="/login"
-							element={<Login loginUser={handleLoginUser} />}
-						/>
-						<Route
-							path="/register"
-							element={
-								<Register registerUser={handleRegisterUser} />
-							}
-						/>
-						<Route
-							path="/write"
-							element={
-								<WritePost createPost={handleCreatePost} />
-							}
-						/>
-					</Route>
-				</Routes>
-			</Router>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar logoutUser={handleLogoutUser} />}>
+            <Route
+              path="/"
+              element={
+                <Home
+                  users={users}
+                  deletePost={handleDeletePost}
+                  likePost={handleLikePost}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={<Login loginUser={handleLoginUser} />}
+            />
+            <Route
+              path="/register"
+              element={<Register registerUser={handleRegisterUser} />}
+            />
+            <Route
+              path="/write"
+              element={<WritePost createPost={handleCreatePost} />}
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/pages/WritePost.jsx
+++ src/pages/WritePost.jsx
@@ -1,54 +1,40 @@
 import React, { useRef } from "react";
 import { Container, Typography, TextField, Button, Paper } from "@mui/material";
-import { useSelector } from "react-redux";
 const WritePost = ({ createPost }) => {
-	const loggedInUser = useSelector((state) => state.users.loggedInUser);
+  const formRef = useRef(null);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    createPost(data.get("title"), data.get("text"));
+    formRef.current.reset();
+  };
 
-	const formRef = useRef(null);
-	const onSubmit = (e) => {
-		e.preventDefault();
-		const data = new FormData(e.target);
-		createPost(data.get("title"), data.get("text"), loggedInUser.username);
-		formRef.current.reset();
-	};
-
-	return (
-		<Container component="main" maxWidth="xs">
-			<Typography variant="h4" marginBottom={"10px"}>
-				Write Post
-			</Typography>
-			<Paper
-				style={{
-					margin: "auto",
-					padding: "1rem",
-				}}
-			>
-				<form onSubmit={onSubmit} ref={formRef}>
-					<TextField
-						label="Title"
-						name="title"
-						fullWidth
-						margin="normal"
-					/>
-					<TextField
-						label="Text"
-						name="text"
-						multiline
-						rows={8}
-						fullWidth
-					/>
-					<Button
-						type="submit"
-						variant="contained"
-						color="primary"
-						sx={{ marginTop: "13px" }}
-					>
-						Publish
-					</Button>
-				</form>
-			</Paper>
-		</Container>
-	);
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography variant="h4" marginBottom={"10px"}>
+        Write Post
+      </Typography>
+      <Paper
+        style={{
+          margin: "auto",
+          padding: "1rem",
+        }}
+      >
+        <form onSubmit={onSubmit} ref={formRef}>
+          <TextField label="Title" name="title" fullWidth margin="normal" />
+          <TextField label="Text" name="text" multiline rows={8} fullWidth />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ marginTop: "13px" }}
+          >
+            Publish
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default WritePost;
