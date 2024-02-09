88b64f32aebd158efb7c2c4fc49340d52fd26e4b
Ata Mohammadi <atasmohammadi@gmail.com>
2024-02-10 00:38:53 +0100

    move actions to respective pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,61 +1,56 @@
 import React from "react";
 import { Container, Typography, TextField, Button, Paper } from "@mui/material";
 import { useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { loginUser } from "./redux/reducers/usersSlice";
 
 const Login = () => {
-	const users = useSelector((state) => state.users.users);
-	const dispatch = useDispatch();
-	const formRef = useRef(null);
-	const onSubmit = (e) => {
-		e.preventDefault();
-		const data = new FormData(e.target);
-		handleLoginUser(data.get("email"), data.get("password"));
-		formRef.current.reset();
-	};
+  const dispatch = useDispatch();
+  const formRef = useRef(null);
 
-	const handleLoginUser = (email, password) => {
-		dispatch(loginUser({ email, password }));
-	};
+  const handleLoginUser = (email, password) => {
+    dispatch(loginUser({ email, password }));
+  };
 
-	return (
-		<Container component="main" maxWidth="xs">
-			<Typography variant="h4" marginBottom={"10px"}>
-				Login
-			</Typography>
-			<Paper
-				style={{
-					margin: "auto",
-					padding: "1rem",
-				}}
-			>
-				<form onSubmit={onSubmit} ref={formRef}>
-					<TextField
-						label="Email"
-						name="email"
-						fullWidth
-						margin="normal"
-					/>
-					<TextField
-						label="Password"
-						name="password"
-						type="password"
-						fullWidth
-						margin="normal"
-					/>
-					<Button
-						variant="contained"
-						color="primary"
-						type="submit"
-						sx={{ marginTop: "8px" }}
-					>
-						Login
-					</Button>
-				</form>
-			</Paper>
-		</Container>
-	);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    handleLoginUser(data.get("email"), data.get("password"));
+    formRef.current.reset();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography variant="h4" marginBottom={"10px"}>
+        Login
+      </Typography>
+      <Paper
+        style={{
+          margin: "auto",
+          padding: "1rem",
+        }}
+      >
+        <form onSubmit={onSubmit} ref={formRef}>
+          <TextField label="Email" name="email" fullWidth margin="normal" />
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            sx={{ marginTop: "8px" }}
+          >
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,83 +1,70 @@
 import React, { useRef, useEffect } from "react";
 import {
-	Container,
-	Typography,
-	TextField,
-	Button,
-	ThemeProvider,
-	Paper,
+  Container,
+  Typography,
+  TextField,
+  Button,
+  ThemeProvider,
+  Paper,
 } from "@mui/material";
 import theme from "../components/Theme";
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../redux/reducers/usersSlice";
 
 const Register = () => {
-	const users = useSelector((state) => state.users.users);
-	const dispatch = useDispatch();
-	const formRef = useRef(null);
-	const onSubmit = (e) => {
-		e.preventDefault();
-		const data = new FormData(e.target);
-		handleRegisterUser(
-			data.get("username"),
-			data.get("email"),
-			data.get("password")
-		);
-		formRef.current.reset();
-	};
+  const dispatch = useDispatch();
+  const formRef = useRef(null);
 
-	useEffect(() => {
-		console.log("users", users);
-	}, [users]);
+  const handleRegisterUser = (username, email, password) => {
+    dispatch(registerUser({ username, email, password }));
+  };
 
-	const handleRegisterUser = (username, email, password) => {
-		dispatch(registerUser({ username, email, password }));
-	};
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    handleRegisterUser(
+      data.get("username"),
+      data.get("email"),
+      data.get("password")
+    );
+    formRef.current.reset();
+  };
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Container component="main" maxWidth="xs">
-				<Typography variant="h4" marginBottom={"10px"}>
-					Register
-				</Typography>
-				<Paper
-					style={{
-						margin: "auto",
-						padding: "1rem",
-					}}
-				>
-					<form onSubmit={onSubmit} ref={formRef}>
-						<TextField
-							label="Username"
-							name="username"
-							fullWidth
-							margin="normal"
-						/>
-						<TextField
-							label="Email"
-							name="email"
-							fullWidth
-							margin="normal"
-						/>
-						<TextField
-							label="Password"
-							name="password"
-							type="password"
-							fullWidth
-							margin="normal"
-						/>
-						<Button
-							variant="contained"
-							color="primary"
-							type="submit"
-						>
-							Register
-						</Button>
-					</form>
-				</Paper>
-			</Container>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Typography variant="h4" marginBottom={"10px"}>
+          Register
+        </Typography>
+        <Paper
+          style={{
+            margin: "auto",
+            padding: "1rem",
+          }}
+        >
+          <form onSubmit={onSubmit} ref={formRef}>
+            <TextField
+              label="Username"
+              name="username"
+              fullWidth
+              margin="normal"
+            />
+            <TextField label="Email" name="email" fullWidth margin="normal" />
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+              margin="normal"
+            />
+            <Button variant="contained" color="primary" type="submit">
+              Register
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default Register;
